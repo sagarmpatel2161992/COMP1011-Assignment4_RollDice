f58607cf75f51cff5b7b63d8d021db06e8714f4b
sagarmpatel2161992 <sagarmpatel2161992@yahoo.com>
2015-03-17 17:25:42 -0700

    Roll Dice In  Working Condition

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dice1,dice2,sum=0;
+            int[] sumArray = new int[11];
+            Random rand = new Random();
+            for (int i = 0; i < 3600; i++)
+            {
+                dice1 = rand.Next(1, 7);
+                dice2 = rand.Next(1, 7);
+                sum = dice1 + dice2;
+                sumArray[sum - 2]++;
+            }
+            for(int i=0;i<11;i++)
+            {
+                Console.WriteLine("Sum " + (i + 2) + ":" + sumArray[i]);                
+            }
+            Console.ReadLine();
+        }
+    }
+}
